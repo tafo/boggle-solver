fc7de708f1a553cbdd1baf83a95d71b9632cc9a9
Tayfun Yirdem <tayfunyirdem@gmail.com>
2020-07-26 20:14:46 +0300

    Add extensions

---
+++ BoggleSolver.Library/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace BoggleSolver.Library
+{
+    public static class Extensions
+    {
+        public static string Path(this string dictionary)
+        {
+            return $"{Directory.GetCurrentDirectory()}/Dictionary/{dictionary}.txt";
+        }
+
+        public static string[] ToArray(this string dictionary)
+        {
+            return File.ReadAllLines(dictionary.Path());
+        }
+    }
+}
