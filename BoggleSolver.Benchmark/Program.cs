c5db001b4330ffd09f3b6ed2ae59e628bb40ef89
Tayfun Yirdem <tayfunyirdem@gmail.com>
2020-07-27 06:16:27 +0300

    Add Benchmark project

---
+++ BoggleSolver.Benchmark/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace BoggleSolver.Benchmark
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.ReadLine();
+            BenchmarkRunner.Run(typeof(SolverBenchmark));
+            Console.ReadLine();
+        }
+    }
+}
